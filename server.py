15e15875743b7aacaa0626f6546b93f468581ddb
AConcrnedCitizen <conall.barry@gmail.com>
2022-08-12 08:16:04 +1000

    k

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    latest = 'latest.jpg'
+    return render_template('index.html', latest=latest)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
