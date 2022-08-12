e8810d9d5c00205dbc25c21852661e15bad42c4d
AConcrnedCitizen <conall.barry@gmail.com>
2022-08-12 12:45:44 +1000

    '

--- server.py
+++ server.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, url_for
+#import takephoto
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
-def lionel(): 
+def page(): 
     return render_template('index.html')
+
+
+@app.route('/background_process_test')
+def background_process_test():
+    #print ("SCREAM")
+    return ("nothing")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
