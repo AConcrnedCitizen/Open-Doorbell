69defa32b75218fba41ef619545dfb2fae581a23
AConcrnedCitizen <conall.barry@gmail.com>
2022-08-12 12:22:19 +1000

    r

--- server.py
+++ server.py
@@ -1,13 +1,9 @@
-from flask import Flask
-from flask import render_template
-import os
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    latest = 'latest.jpg'
-    return render_template('index.html', latest=latest)
+@app.route('/', methods=['GET', 'POST'])
+def lionel(): 
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
