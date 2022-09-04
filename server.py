7dd6a6436df791ada4a4a50bfd0c964b73e53d75
AConcrnedCitizen <80365234+AConcrnedCitizen@users.noreply.github.com>
2022-09-04 23:24:45 +1000

    audio

--- server.py
+++ server.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template, url_for
+from crypt import methods
+from urllib import request
+from flask import Flask, render_template, url_for, request
 import takephoto as photo
-
-
+import time
+import tts
+from pydub import AudioSegment
+from pydub.playback import play
 
 app = Flask(__name__)
 
+shuttersound = AudioSegment.from_file("shutter.mp3", format="mp3")
 
-@app.route('/', methods=['GET', 'POST'])
-def page(): 
+
+@app.route('/')
+def page():
     return render_template('index.html')
 
 
-@app.route('/background_process_test')
-def background_process_test():
+@app.route('/take_photo')
+def takephoto():
     photo.takephoto()
+    play(shuttersound)
+    time.sleep(5)
     return("nothing")
+
+
+@app.route('/', methods=['POST'])
+def speak():
+    text = request.form['text']
+    tts.speak(text)
+    print("text")
+    p_text = text.upper()
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-    
---
+++ tts.py
@@ -0,0 +1,9 @@
+import pyttsx3
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.setProperty('volume', 1.0)
+    engine.setProperty('rate', 125)
+    engine.say(str(text))
+    engine.runAndWait()
