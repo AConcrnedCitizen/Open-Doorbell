d7739033b6ece24cea4104a2a8e182e538a8cf21
Jack <github@accounts.eddster.com>
2022-09-20 23:02:00 +1000

    shit tons

---
+++ static/live.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    //connect to the socket server.
+    var x = document.getElementById("audio"); 
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/live');
+    //receive details from server
+    socket.on('buttonpressed', function (msg) {
+        console.log(msg);
+        x.play();
+    });
+
+});
--- static/script.js
+++ static/script.js
@@ -1,22 +1,22 @@
-function clicked_img(img, fp) {
+function clicked_img(img,fp){
     console.log(img.src);
 
-    var top = document.getElementById('top')
+    var top=document.getElementById('top')
 
     top.src = img.src;
 
-    top.hidden = false;
+    top.hidden=false;
 
 
-    if (img.naturalWidth < screen.width * 0.6 && img.naturalHeight < screen.height * 0.6) {
+    if (img.naturalWidth<screen.width*0.6 && img.naturalHeight<screen.height*0.6) {
 
-        top.width = img.naturalWidth;
-        top.height = img.naturalHeight;
+      top.width=img.naturalWidth;
+      top.height=img.naturalHeight;
 
     } else {
 
-        top.width = screen.width * 0.6;
-        top.height = img.naturalHeight / img.naturalWidth * top.width;
+      top.width=screen.width*0.6;
+      top.height=img.naturalHeight/img.naturalWidth*top.width;
 
     }
 
@@ -24,7 +24,7 @@ function clicked_img(img, fp) {
 }
 
 
-function do_close() {
-    document.getElementById('top').hidden = true;
-    document.getElementById('close').hidden = true;
+function do_close(){
+document.getElementById('top').hidden=true;
+document.getElementById('close').hidden=true;
 }
